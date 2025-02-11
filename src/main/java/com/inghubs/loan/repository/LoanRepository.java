70e7e15f83a337b00f1a886f2f66756268560682
Cengiz Bilgiç <80578925+litenin-admin@users.noreply.github.com>
2025-02-11 15:58:44 +0300

    Update LoanRepository.java

--- src/main/java/com/inghubs/loan/repository/LoanRepository.java
+++ src/main/java/com/inghubs/loan/repository/LoanRepository.java
@@ -2,11 +2,9 @@ package com.inghubs.loan.repository;
 
 import com.inghubs.loan.model.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface LoanRepository extends JpaRepository<Loan, Long> {
 
     List<Loan> findByCustomerGuid(String customerGuid);
